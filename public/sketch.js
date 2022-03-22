6802873ed2f3bdb3578c6cd1618e1dcdddd10fff
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-03-22 16:58:32 +0000

    deploy: 807549f560f2e056786d9eb402a06573ef846185

---
+++ public/sketch.js
@@ -0,0 +1,22 @@
+/// <reference path="../node_modules/@types/p5/global.d.ts" />
+
+/*
+
+Data from: https://insights.stackoverflow.com/survey/
+
+
+
+Lets go with:
+Which languages lead to other langauges the most
+
+*/
+
+function setup() {
+    createCanvas(800,600);
+}
+
+function draw() {
+    background(225);
+    
+    
+}
