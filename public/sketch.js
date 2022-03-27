bfb7fe5cc8ef5cd67c6adce042a1518153fbde43
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-27 18:03:53 -0400

    Rendering is great

--- public/sketch.js
+++ public/sketch.js
@@ -52,8 +52,6 @@ function setup() {
 
     side_width = width * 1 / 5;
 
-    while (data == null);
-    
     lang_height = height / Object.keys(data).length;
 }
 
@@ -70,23 +68,77 @@ function updateOpacities() {
         element['opacity_left'] = interpolate(element['opacity_left'], hover_index_left == -1 ? OPACITY_DEFAULT : index == hover_index_left ? OPACITY_SHOWN : OPACITY_HIDDEN);
         element['opacity_right'] = interpolate(element['opacity_right'], hover_index_right == -1 ? OPACITY_DEFAULT : index == hover_index_right ? OPACITY_SHOWN : OPACITY_HIDDEN);
         element['values'].forEach(element_2 => {
-            opacity = hover_index_left == -1 && hover_index_right == -1 ? OPACITY_DEFAULT : index == hover_index_left || data.indexOf(element_2['lang']) == hover_index_right ? OPACITY_SHOWN : OPACITY_HIDDEN;
-            element_2['opacity'] = interpolate(element_2['opacity'],opacity);
+            opacity = hover_index_left == -1 && hover_index_right == -1 ? OPACITY_DEFAULT * element_2['value'] : index == hover_index_left || findLanguageIndex(element_2['lang']) == hover_index_right ? OPACITY_SHOWN : 0;
+            element_2['opacity'] = interpolate(element_2['opacity'], opacity);
         });
     });
 }
 
 function render() {
     noStroke();
-    data.forEach((element,index) => {
-        fill_color = color(element['color']);
+    data.forEach((element, index) => {
+        noStroke();
+
+        // Render the Squares
+        var fill_color = color(element['color']);
         fill_color.setAlpha(255 * element['opacity_left']);
         fill(fill_color);
-        rect(0,lang_height * index,side_width,lang_height);
+        rect(0, lang_height * index, side_width, lang_height);
         fill_color.setAlpha(255 * element['opacity_right']);
         fill(fill_color);
-        rect(width - side_width,lang_height * index,side_width,lang_height);
+        rect(width - side_width, lang_height * index, side_width, lang_height);
+
+        // Render the Text
+        stroke('white');
+        fill('black');
+        text(element['lang'], side_width * 0.1, (index + 1) * lang_height - lang_height * 0.2);
+        textSize(15);
+        textStyle(BOLD);
+        text(element['lang'], width - side_width * 0.4, (index + 1) * lang_height - lang_height * 0.2);
+        noStroke();
+
+
+
+        // Render the connectors
+        element['values'].forEach((element_2) => {
+            const half_height = lang_height * element_2['value'] / 2;
+            const goal_index = findLanguageIndex(element_2['lang']);
+            const left_height = lang_height * index + lang_height / 2;
+            const right_height = lang_height * goal_index + lang_height / 2;
+
+            fill_color.setAlpha(255 * element_2['opacity']);
+            fill(fill_color);
+            quad(side_width, left_height - half_height, side_width, left_height + half_height, width - side_width, right_height + half_height, width - side_width, right_height - half_height);
+
+
+            noStroke();
+        });
+
     });
+
+    // render the text
+    data.forEach((element, index) => {
+        if (index == hover_index_left) {
+            element['values'].forEach((element_2) => {
+                stroke('white');
+                fill('black');
+                textSize(15);
+                textStyle(BOLD);
+                text(new String(element_2['value']).substring(0,4) + "%", width - side_width * 0.9, (findLanguageIndex(element_2['lang']) + 1) * lang_height - lang_height * 0.2)
+            });
+        } else if(hover_index_right != -1) {
+
+        }
+    });
+}
+
+function findLanguageIndex(language) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i]['lang'] == language) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 function interpolate(initial, goal) {
